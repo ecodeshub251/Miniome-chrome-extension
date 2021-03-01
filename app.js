34792e8b92f1d046456ea90e100ee938f36c2eb3
ecodeshub251 <amirkhan251@outlook.com>
2021-03-01 18:30:55 +0530

    add task form

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
 var mainbutton = document.querySelector('.main-icon');
+var todoform = document.querySelector('.todo-form');
 var icons = document.querySelectorAll('.icon');
 mainbutton.addEventListener('click', function (e) {
     icons.forEach(function (icon) {
         icon.classList.toggle('invisible');
     });
 });
+todoform.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var taskname = todoform['task'].value;
+    addTask(taskname);
+    todoform['task'].value = '';
+});
+getTasks();
+function addTask(taskname) {
+    var taskArray = getTasks();
+    if (taskArray === null) {
+        taskArray = [];
+    }
+    else {
+        taskArray;
+    }
+    // taskArray.push(taskname)
+    console.log(taskArray);
+    // localStorage.setItem('task', JSON.stringify(taskname))
+}
+function getTasks() {
+    return JSON.parse(localStorage.getItem('task'));
+}
